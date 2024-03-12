aa5b38ed8d9fd938c3f137e3331ca84164762d84
Kimmo Sääskilahti <kimmo.saaskilahti@sanoma.com>
2024-03-12 17:49:10 +0200

    Clean-up:g

--- app/components/decipher.tsx
+++ app/components/decipher.tsx
@@ -3,15 +3,19 @@ import { Result } from "../hooks/useDecipher";
 export default function Decipher({ prompt, result, error, loading }: { prompt?: string, result?: Result, error?: Error, loading: boolean }) {
     return (
         <div>
-            <p>
+            {/* {prompt && <p>
                 Prompt: {prompt}
-            </p>
-            <p>
-                Result: {result?.species} {result?.amount}
-            </p>
-            <p>
+            </p>} */}
+            {result && <p>
+                Result:
+                <ol>
+                    <li>Laji: {result?.species}</li>
+                    <li>Määrä: {result?.amount}</li>
+                </ol>
+            </p>}
+            {error && <p>
                 Error: {error?.message}
-            </p>
+            </p>}
         </div>
     )
 }
