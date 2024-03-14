f94b4b76424ef716898e01dab793c3efa9f7c64a
Kimmo Sääskilahti <kimmo.saaskilahti@sanoma.com>
2024-03-14 14:55:25 +0200

    Clean-up decipher too.

--- app/components/decipher.tsx
+++ app/components/decipher.tsx
@@ -2,20 +2,26 @@ import { Result } from "../hooks/useDecipher";
 
 export default function Decipher({ prompt, result, error, loading }: { prompt?: string, result?: Result, error?: Error, loading: boolean }) {
     return (
-        <div>
-            {/* {prompt && <p>
-                Prompt: {prompt}
-            </p>} */}
-            {result && <p>
-                Result:
-                <ol>
-                    <li>Laji: {result?.species}</li>
-                    <li>Määrä: {result?.amount}</li>
-                </ol>
-            </p>}
-            {error && <p>
-                Error: {error?.message}
-            </p>}
-        </div>
+        <div className="toast toast-start toast-bottom">
+            {loading && (
+                <div className="alert alert-info">
+                    <p className="font-bold">Prosessoidaan syötettä...</p>
+                </div>)
+            }
+            {error && (
+                <div className="alert alert-error">
+                    <h3 className="font-bold">Virhe: lajihavainnon luonti epäonnistui</h3>
+                </div>
+            )}
+            {result && (
+                <div className="alert alert-success">
+                    <div>
+                        <h3 className="font-bold">Lajihavainnon luonti onnistui</h3>
+                        <div className="text-xs">Laji: {result.species}</div>
+                        <div className="text-xs">Määrä: {result.amount}</div>
+                    </div>
+                </div>)
+            }
+        </div >
     )
 }
