e1c53c26430ec6a663fba61017657e38aba22e7f
Kimmo Sääskilahti <kimmo.saaskilahti@silo.ai>
2024-03-23 08:35:26 +0200

    Improve style and prompt.

--- app/components/decipher.tsx
+++ app/components/decipher.tsx
@@ -10,18 +10,44 @@ export default function Decipher({
   loading: boolean;
 }) {
   return (
-    <div className="w-full">
+    <div className="w-full text-xs">
       {loading && (
         <div className="alert alert-info">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-current shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            className="stroke-current shrink-0 w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            ></path>
+          </svg>
           <p className="font-bold">Prosessoidaan syötettä...</p>
         </div>
       )}
       {error && !loading && (
         <div className="alert alert-error">
-          <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="stroke-current shrink-0 h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
           <div>
-            <h3 className="font-bold">Virhe: lajihavainnon luonti epäonnistui</h3>
+            <h3 className="font-bold">
+              Virhe: lajihavainnon luonti epäonnistui
+            </h3>
             <div className="text-xs">{error.title}</div>
             <div className="text-xs">{error.detail}</div>
           </div>
@@ -29,7 +55,19 @@ export default function Decipher({
       )}
       {result && !loading && (
         <div className="alert alert-success">
-          <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="stroke-current shrink-0 h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
           <div>
             <h3 className="font-bold">Lajihavainnon luonti onnistui</h3>
             <div className="text-xs">Laji: {result.species.finnishName}</div>
--- app/prompting/prompt.ts
+++ app/prompting/prompt.ts
@@ -1,5 +1,5 @@
 export default function buildPrompt(userInput: string) {
-    const description = `Tehtävä on määrittää lintulaji ja määrä käyttäjän
+  const description = `Tehtävä on määrittää lintulaji ja määrä käyttäjän
 luonnollisella kielellä antamasta syötteestä. Käyttäjän syöte on teksti kuten
 'kolme töyhtökiurua' ja vastaus on JSON-objekti jossa on laji ja määrä
 kuten '{ "laji": "töyhtökiuru", "määrä": 3 }'.
@@ -10,45 +10,49 @@ Käytä lajin nimenä yksikkömuotoa ja määränä kokonaislukua.
 
 Jos lintulajia ja määrää ei voida määrittää, vastauksen täytyy olla tyhjä JSON-objekti '{}'.
 
-Jos lintulaji on kirjoitettu väärin, korjaa se oikeaan muotoon.
-`
-    const examples = [
-        {
-            input: "Kolme varista",
-            response: "{ \"laji\": \"varis\", \"määrä\": 3 }"
-        },
-        {
-            input: "Kaksi harakkaa",
-            response: "{ \"laji\": \"harakka\", \"määrä\": 2 }"
-        },
-        {
-            input: "Varpunen",
-            response: "{ \"laji\": \"varpunen\", \"määrä\": 1 }"
-        },
-        {
-            input: "Kiitos kun katsoit!",
-            response: "{}"
-        },
-        {
-            input: "Iso koskelo",
-            response: `{ "laji": "isokoskelo", "määrä": 1 }`
-        },
-        {
-            input: "Kaksi käpytikkoa",
-            response: `{ "laji": "käpytikka", "määrä": 2 }`
-        },
-        {
-            input: "Viiru pöllä",
-            response: `{ "laji": "viirupöllö", "määrä": 1 }`
-        },
-    ]
+Jos lintulaji on kirjoitettu väärin, korjaa se oikeaan muotoon. Korjauksen pitää olla pieni, enintään kolme kirjainta.
+`;
+  const examples = [
+    {
+      input: "Kolme varista",
+      response: '{ "laji": "varis", "määrä": 3 }',
+    },
+    {
+      input: "Kaksi harakkaa",
+      response: '{ "laji": "harakka", "määrä": 2 }',
+    },
+    {
+      input: "Varpunen",
+      response: '{ "laji": "varpunen", "määrä": 1 }',
+    },
+    {
+      input: "Kiitos kun katsoit!",
+      response: "{}",
+    },
+    {
+      input: "Iso koskelo",
+      response: `{ "laji": "isokoskelo", "määrä": 1 }`,
+    },
+    {
+      input: "Kaksi käpytikkoa",
+      response: `{ "laji": "käpytikka", "määrä": 2 }`,
+    },
+    {
+      input: "15 rautiaista",
+      response: `{ "laji": "rautiainen", "määrä": 15 }`,
+    },
+    {
+      input: "Viiru pöllä",
+      response: `{ "laji": "viirupöllö", "määrä": 1 }`,
+    },
+  ];
 
-    const systemPrompt = `${description}
+  const systemPrompt = `${description}
 Esimerkkejä:
 
-${examples.map(example =>
-        `Syöte: ${example.input}\n${example.response}`
-    ).join("\n\n")}`
+${examples
+  .map((example) => `>>> ${example.input}\n${example.response}`)
+  .join("\n\n")}`;
 
-    return { system: systemPrompt, user: `Syöte: ${userInput}` }
+  return { system: systemPrompt, user: `>>> ${userInput}` };
 }
