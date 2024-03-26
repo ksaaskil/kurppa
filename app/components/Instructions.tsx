f5d999f0b4f882088d185d61942a5fa4384ef332
Kimmo Sääskilahti <ksaaskil@users.noreply.github.com>
2024-03-26 16:41:22 +0200

    Use container for style. (#9)

--- app/components/Instructions.tsx
+++ app/components/Instructions.tsx
@@ -1,15 +1,33 @@
+import { useEffect, useState } from "react";
+
+const EXAMPLES = [
+  "kolme varista",
+  "viisitoista kiurua",
+  "varpunen",
+  "kaksi kottaraista",
+]
+
 export default function Instructions() {
+
+  const [example, setExample] = useState("");
+
+  useEffect(() => {
+    setExample(EXAMPLES[Math.floor(Math.random() * EXAMPLES.length)])
+  }, [])
+
   return (
-    <div className="card card-bordered w-96 shadow-xl border-primary">
+    <>
+    { example && (<div className="card card-bordered w-full shadow-xl border-primary">
       <div className="card-body flex flex-row text-center">
-        <div className="grow prose">
+        <div className="grow prose text-xs lg:text-base">
           <span>1. Paina mikrofonikuvaketta</span>
           <br />
-          <span>2. Sano rauhallisesti {`"kolme varista"`}</span>
+          <span>2. Sano esimerkiksi {`"${example}"`}</span>
           <br />
           <span>3. Lopeta nauhoitus painamalla kuvaketta</span>
         </div>
       </div>
-    </div>
+    </div>) }
+    </>
   );
 }
