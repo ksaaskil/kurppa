01132944e691e3bfe60d94d49d577fd72b1bd52c
Kimmo Sääskilahti <ksaaskil@users.noreply.github.com>
2024-03-28 08:52:42 +0200

    Use refs instead of let (#11)

--- app/hooks/useRecordVoice.ts
+++ app/hooks/useRecordVoice.ts
@@ -1,85 +1,79 @@
 "use client";
-import { useEffect, useState, useRef } from "react";
-import { blobToBase64 } from "@/app/utils/blobToBase64";
+import { useState, useRef } from "react";
 import { createMediaStream } from "@/app/utils/createMediaStream";
 
-let mediaRecorder: MediaRecorder | null = null;
-let stream: MediaStream | null = null;
-
 function useRecordVoice() {
-  const [state, setState] = useState("Waiting");
   const [audio, setAudio] = useState(undefined as Blob | undefined);
   const [recording, setRecording] = useState(false);
   const isRecording = useRef(false);
+
+  const stream = useRef<MediaStream | null>(null);
+  const mediaRecorder = useRef<MediaRecorder | null>(null);
   const chunks = useRef([] as Blob[]);
 
   function close() {
-    const tracks = stream?.getTracks() || [];
+    const tracks = stream.current?.getTracks() || [];
     console.log(`Stopping ${tracks.length} tracks`);
     for (const track of tracks) {
       track.stop();
     }
-    stream = null;
-    mediaRecorder = null;
+    stream.current = null;
+    mediaRecorder.current = null;
   }
 
   async function initialize() {
     if (typeof window !== "undefined") {
-      const stream_ = await navigator.mediaDevices.getUserMedia({
+      stream.current = await navigator.mediaDevices.getUserMedia({
         audio: true,
       });
-      initializeMediaRecorder(stream_);
-      stream = stream_
+      initializeMediaRecorder(stream.current);
     }
   }
 
   const startRecording = async () => {
     console.log(`Starting recording`);
     await initialize();
-    if (mediaRecorder) {
+    if (mediaRecorder.current) {
       isRecording.current = true;
-      mediaRecorder.start();
+      mediaRecorder.current?.start();
       setRecording(true);
     }
-    setState("Recording...");
   };
 
   const stopRecording = () => {
     console.log(`Stop recording`);
-    if (mediaRecorder) {
+    if (mediaRecorder.current) {
       isRecording.current = false;
-      mediaRecorder.stop();
+      mediaRecorder.current?.stop();
       setRecording(false);
       close();
     }
-    setState("Stopped recording");
   };
 
-  const initializeMediaRecorder = (stream: any) => {
+  const initializeMediaRecorder = (stream: MediaStream) => {
     console.log(`Initializing media recorder...`);
-    mediaRecorder = new MediaRecorder(stream);
+    mediaRecorder.current = new MediaRecorder(stream);
 
-    mediaRecorder.onstart = () => {
+    mediaRecorder.current.onstart = () => {
       console.log(`Media recorder onstart`);
       createMediaStream(stream);
       chunks.current = [];
     };
 
-    mediaRecorder.ondataavailable = (ev) => {
+    mediaRecorder.current.ondataavailable = (ev) => {
       chunks.current.push(ev.data);
     };
 
-    mediaRecorder.onstop = async () => {
+    mediaRecorder.current.onstop = async () => {
       console.log(`Media recorder onstop`);
-      console.log(`Converting ${chunks.current.length} chunks`);
-      const type = mediaRecorder?.mimeType;
+      const type = mediaRecorder.current?.mimeType;
+      console.log(`Creating audio blob of type ${type} from ${chunks.current.length} chunks`);
       const audioBlob = new Blob(chunks.current, { type });
       setAudio(audioBlob);
     };
-
   };
 
-  return { recording, startRecording, stopRecording, audio, state };
+  return { recording, startRecording, stopRecording, audio };
 }
 
 export { useRecordVoice };
