7f2b6845997fbc5701219968d67937dc8dd62582
Kimmo Sääskilahti <kimmo.saaskilahti@silo.ai>
2024-03-29 13:47:31 +0200

    Use location.

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -1,9 +1,18 @@
+import { useContext } from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { LocationContext } from "./Providers";
 
 export default function Map() {
+  const { location } = useContext(LocationContext);
+
+  const centerLocation = location || {
+    latitude: 60.1960327054566,
+    longitude: 25.059909619299244,
+  };
+
   return (
     <MapContainer
-      center={[51.505, -0.09]}
+      center={[centerLocation.latitude, centerLocation.longitude]}
       zoom={13}
       scrollWheelZoom={false}
       style={{ height: 400, width: "100%" }}
@@ -12,11 +21,11 @@ export default function Map() {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      {location && (
+        <Marker position={[location.latitude, location.longitude]}>
+          <Popup>Nykyinen sijaintisi</Popup>
+        </Marker>
+      )}
     </MapContainer>
   );
 }
