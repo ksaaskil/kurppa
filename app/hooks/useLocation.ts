b773d581bf51ee997909e37edbd86feb67540311
Kimmo Sääskilahti <kimmo.saaskilahti@silo.ai>
2024-03-29 12:55:28 +0200

    Store value to local storage.

--- app/hooks/useLocation.ts
+++ app/hooks/useLocation.ts
@@ -1,15 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export function useLocation() {
   const [enabled, setEnabled] = useState(false);
 
   async function start() {
+    console.log(`Starting using location`);
     setEnabled(true);
+    window.localStorage.setItem("use-location", "true");
   }
 
   async function stop() {
+    console.log(`Stopping using location`);
     setEnabled(false);
+    window.localStorage.setItem("use-location", "false");
   }
+
+  useEffect(() => {
+    const enabled = window.localStorage.getItem("use-location") === "true";
+    if (enabled) {
+      start();
+    }
+  }, []);
 
   return { enabled, start, stop };
 }
