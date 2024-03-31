76b0278e113021a183a14c7dece13274b555aed7
Kimmo Sääskilahti <kimmo.saaskilahti@silo.ai>
2024-03-31 11:58:56 +0300

    Add warning of using Safari.

--- app/components/Instructions.tsx
+++ app/components/Instructions.tsx
@@ -5,29 +5,59 @@ const EXAMPLES = [
   "viisitoista kiurua",
   "varpunen",
   "kaksi kottaraista",
-]
+];
+
+const useUserAgent = () => {
+  const [safari, setSafari] = useState(false);
+
+  useEffect(() => {
+    const usesSafari =
+      (navigator.vendor.match(/apple/i) &&
+        !navigator.userAgent.match(/crios/i) &&
+        !navigator.userAgent.match(/fxios/i) &&
+        !navigator.userAgent.match(/Opera|OPT\//)) ||
+      false;
+    setSafari(usesSafari);
+  }, []);
+
+  return { isSafari: safari };
+};
 
 export default function Instructions() {
-
   const [example, setExample] = useState("");
 
   useEffect(() => {
-    setExample(EXAMPLES[Math.floor(Math.random() * EXAMPLES.length)])
-  }, [])
+    setExample(EXAMPLES[Math.floor(Math.random() * EXAMPLES.length)]);
+  }, []);
+
+  const { isSafari } = useUserAgent();
 
   return (
     <>
-    { example && (<div className="card card-bordered w-full shadow-xl border-primary">
-      <div className="card-body flex flex-row text-center">
-        <div className="grow prose text-xs lg:text-base">
-          <span>1. Paina mikrofonikuvaketta</span>
-          <br />
-          <span>2. Sano esimerkiksi {`"${example}"`}</span>
-          <br />
-          <span>3. Lopeta nauhoitus painamalla kuvaketta</span>
+      {example && (
+        <div className="card card-bordered w-full shadow-xl border-primary">
+          <div className="card-body flex flex-col text-center">
+            {isSafari && (
+              <span>
+                <span className="font-bold">
+                  HUOM! Safari ei kuulu tuettuihin selaimiin.
+                </span>
+                <br />
+                <span className="font-bold">
+                  Kurppa toimii parhaiten Firefoxilla ja Chromella.
+                </span>
+              </span>
+            )}
+            <div className="grow prose text-xs lg:text-base">
+              <span>1. Paina mikrofonikuvaketta</span>
+              <br />
+              <span>2. Sano esimerkiksi {`"${example}"`}</span>
+              <br />
+              <span>3. Lopeta nauhoitus painamalla kuvaketta</span>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>) }
+      )}
     </>
   );
 }
