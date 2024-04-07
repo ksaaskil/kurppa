b6eb936aab488283698a3581f2ecd617827ca426
Kimmo Sääskilahti <kimmo.saaskilahti@silo.ai>
2024-04-07 11:14:56 +0300

    Tekstin ymmärtäminen.

--- app/components/DecipherStatus.tsx
+++ app/components/DecipherStatus.tsx
@@ -14,26 +14,24 @@ export default function Decipher({
       {loading && (
         <div className="">
           <p className="font-bold">
-            Prosessoidaan syötettä
+            Tekstin ymmärtäminen
             <span className="ml-2 loading loading-dots loading-sm"></span>
           </p>
         </div>
       )}
-      {error && !loading && (
+      {!loading && error && (
         <div className="">
           <div>
-            <h3 className="font-bold">
-              Virhe: lajihavainnon luonti epäonnistui
-            </h3>
+            <h3 className="font-bold">Tekstin ymmärtäminen epäonnistui</h3>
             <div className="text-xs">{error.title}</div>
             <div className="text-xs">{error.detail}</div>
           </div>
         </div>
       )}
-      {result && !loading && !error && (
+      {!loading && !error && result && (
         <div className="">
           <div>
-            <h3 className="font-bold">Lajihavainnon luonti onnistui</h3>
+            <h3 className="font-bold">Tekstin ymmärtäminen onnistui</h3>
             <div className="text-xs">Laji: {result.species.finnishName}</div>
             <div className="text-xs">Määrä: {result.amount}</div>
           </div>
