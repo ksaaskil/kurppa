03ffacc6da7ea83a5060bb637eec17209da9c28d
Kimmo Sääskilahti <kimmo.saaskilahti@silo.ai>
2024-04-14 17:11:12 +0300

    Add user icon.

--- app/components/LoginButton.tsx
+++ app/components/LoginButton.tsx
@@ -1,21 +1,30 @@
 import { useUser } from "@auth0/nextjs-auth0/client";
 
-const UserIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    strokeWidth={1.5}
-    stroke="currentColor"
-    className="w-6 h-6"
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
-    />
-  </svg>
-);
+const UserIcon = ({ picture }: { picture?: string | null }) => {
+  if (picture) {
+    return (
+      <button className="btn btn-xs btn-ghost">
+        <img src={picture} width={24} height={24} alt="User profile icon" />
+      </button>
+    );
+  }
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-6 h-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+      />
+    </svg>
+  );
+};
 
 export default function LoginButton() {
   const { user } = useUser();
@@ -28,12 +37,15 @@ export default function LoginButton() {
           role="button"
           className="btn btn-circle btn-ghost text-info"
         >
-          <UserIcon />
+          <UserIcon picture={user.picture} />
         </div>
         <ul
           tabIndex={0}
           className="dropdown-content p-1 z-[1] menu shadow bg-base-100"
         >
+          <li>
+            <span>{user.email}</span>
+          </li>
           <li>
             <a href="/api/auth/logout">Kirjaudu ulos</a>
           </li>
